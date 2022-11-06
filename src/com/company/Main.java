931a8dc3c644c811aaaea4b947856ba54e33f412
Aditya <jaiswaladitya1301@gmail.com>
2022-11-06 18:25:24 +0530

    Pattern13

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-            pattern10(5);
+            pattern13(5);
 
         }
         static void pattern8(int n){
@@ -22,8 +22,8 @@ public class Main {
         }
         }
         static void pattern10(int n) {
-        for(int i=0;i<n;i++){
-            for (int k=0;k<n-i;k++){
+        for(int i=0;i<=n;i++){
+            for (int k=0;k<=n-i;k++){
                 System.out.print(" ");
 
             }
@@ -35,4 +35,62 @@ public class Main {
             }
 
         }
+        static void pattern11(int n){
+            for(int i=5;i>=0;--i) {
+                for (int k = 0; k < n-i; k++) {
+                    System.out.print(" ");
+
+                }for (int j = 0; j < 2 * i - 1; j++){
+                    System.out.print("*");
+                }
+                System.out.println("");
+            }
         }
+        static void pattern12(int n){
+            for(int i=0;i<=n;i++){
+                for (int k=0;k<n-i;k++){
+                    System.out.print(" ");
+
+                }
+                for(int j=0;j<2*i-1;j++){
+                    System.out.print("*");
+                }
+                System.out.println("");
+            }
+            for(int i=5;i>=0;--i) {
+                for (int k = 0; k < n-i; k++) {
+                    System.out.print(" ");
+
+                }for (int j = 0; j < 2 * i - 1; j++){
+                    System.out.print("*");
+                }
+                System.out.println("");
+            }
+
+
+        }
+        static void pattern13(int n){
+            for(int i=1;i<=n;i++) {
+                for (int j =1;j<=i; j++){
+                    System.out.print("*");}
+                for (int k =1; k <= n-i; k++) {
+                    System.out.print(" ");
+                }
+                System.out.println("");
+            }
+            for(int i=n;i>0;i--){
+                for (int j=0;j<i-1;j++){
+                    System.out.print("*");
+                }
+                System.out.println("");
+
+            }
+
+
+
+
+}}
+
+
+
+
