ca531743cd38894e5fe8a5e00fee1a677dfeca30
Aditya <jaiswaladitya1301@gmail.com>
2022-11-07 20:03:07 +0530

    Patterns

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-            pattern13(5);
+            pattern16(5);
 
         }
         static void pattern8(int n){
@@ -33,8 +33,17 @@ public class Main {
             System.out.println("");
 
             }
+            for(int i=0;i<=n;i++){
+                for (int k=0;k<=n-i;k++){
+                    System.out.print(" ");
 
-        }
+                }
+                for(int j=0;j<2*i-1;j++){
+                    System.out.print("*");
+                }
+                System.out.println("");
+
+        }}
         static void pattern11(int n){
             for(int i=5;i>=0;--i) {
                 for (int k = 0; k < n-i; k++) {
@@ -84,12 +93,59 @@ public class Main {
                 }
                 System.out.println("");
 
+            }}
+        static void pattern14(int n){
+            for(int i=1;i<=n;i++){
+                for(int j=i;j>=1;j--){
+                    System.out.print(j%2);
+                    }System.out.println();
+            }}
+        static void pattern15(int n){
+          int s=(2*n)-2;
+
+            for(int i=1;i<=n;i++) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(j);
+                }
+                for (int k = 1; k <= s; k++) {
+                    System.out.print(" ");
+                }
+                for (int l = i; l >= 1; l--) {
+                    System.out.print(l);
+                }
+                System.out.println();
+                s=s-2;
+            }}
+
+
+
+
+        static void pattern16(int n){
+        int a=1;
+            for (int i = 1; i <=n ; i++) {
+                for (int j = 1; j <=i ; j++) {
+                    System.out.print(a+" ");
+                    a+=1;
+                } System.out.println();
             }
 
+        }
 
 
 
-}}
+
+
+
+
+
+
+
+}
+
+
+
+
+
 
 
 
